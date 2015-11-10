b34c41235a5c0ce5fe3f36dd534578b79b550839
André Tarnowsky <andre.tarnowsky@joocom.de>
2015-11-10 21:43:09 +0100

    change dependencies

---
+++ emojione/package.js
@@ -0,0 +1,29 @@
+Package.describe({
+  name: 'emojione:emojione',
+  summary: 'Meteor Package of http://www.emojione.com/ set',
+  version: '1.5.2',
+  git: 'https://github.com/Ranks/emojione.git'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0.2.1');
+
+  api.addFiles([
+    'lib/meteor/pre-export.js',
+    'lib/js/emojione.js',
+    'lib/meteor/post-export.js',
+  ]);
+
+  api.use([
+    'blaze',
+    'htmljs',
+    'templating',
+  ], 'client');
+
+  api.addFiles([
+    'lib/meteor/emojione-client.js',
+    'assets/css/emojione.css',
+  ], 'client');
+
+  api.export('emojione');
+});
